0bc2b79a0b4f10ba8012b4911d3a10a43da05ac8
Rose <131393461+Kendi044@users.noreply.github.com>
2025-09-12 19:23:13 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,4 @@
+# relationship_app/views.py
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
@@ -6,11 +7,10 @@ from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 # Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-# Create your views here.
