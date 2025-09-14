7ec08b975207815b1ef06a86ce6496db4018154a
Rose <131393461+Kendi044@users.noreply.github.com>
2025-09-14 20:02:21 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import register
+from .views import book_list, LibraryDetailView
+
+urlpatterns = [
+    # URL for the function-based view
+    path('books/', book_list, name='book-list'),
+    
+    # URL for the class-based view with a dynamic primary key
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+]
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
