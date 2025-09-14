c0fe0009fe748b4e9d2a2d481dcb9bbd286a00c2
Rose <131393461+Kendi044@users.noreply.github.com>
2025-09-14 15:07:49 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, register
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render
 
-urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
-]
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+def register(request):
+    # Your registration logic here
+    pass
