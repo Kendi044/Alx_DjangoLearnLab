673a901a083ffb95dca91e7e5ce8c6cd62704034
Rose <131393461+Kendi044@users.noreply.github.com>
2025-09-14 14:57:28 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 class CustomLoginView(LoginView):
     """
