83b6cdcb17044f3e557fb60664bdbaf511ff2932
Rose <131393461+Kendi044@users.noreply.github.com>
2025-09-26 04:28:21 -0700

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import BookList # Import the view you just created
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Maps the endpoint 'api/books/' (when included in the project) to the BookList view
+    path('books/', BookList.as_view(), name='book-list'), 
+    path('api/', include('api.urls')),
 ]
