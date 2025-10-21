ecd328353e0ac47beaa41d7bff9e5e20be65efba
Rose <131393461+Kendi044@users.noreply.github.com>
2025-10-21 11:24:03 -0700

    Change profile_picture to ImageField for User model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,18 +1,13 @@
-# accounts/models.py
+# accounts/models.py (Updated for ImageField)
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # Additional fields
+    # ... other fields
     bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.URLField(max_length=200, blank=True)
-    # ManyToMany for following (Task 2 will modify this)
-    following = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='followers',
-        blank=True
-    )
-
+    # 🔑 Using ImageField (Requires Pillow and Media setup)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True) 
+    # ... other fields
+    
     def __str__(self):
         return self.username
